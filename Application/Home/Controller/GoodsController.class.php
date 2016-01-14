b85d9606d3f1131156cb5cc0deaa9ed0501c996d
magicbirdyj <12493085@qq.com>
2016-01-14 16:39:26 +0800

    分享按钮

--- Application/Home/Controller/GoodsController.class.php
+++ Application/Home/Controller/GoodsController.class.php
@@ -19,6 +19,10 @@ class GoodsController extends FontEndController {
         $goods=$goodsmodel->table('m_goods t1,m_users t2,m_category t3')->where("t1.user_id=t2.user_id and t1.goods_id=$goods_id and t1.cat_id=t3.cat_id")->field('t1.goods_id,t1.area,t1.goods_name,t1.price,t1.yuan_price,t1.goods_img,t1.goods_img_qita,t1.goods_sex,t1.goods_desc,t1.comment_number,t1.shuxing,t1.score,t3.cat_name,t2.user_name,t1.user_id,t2.weixin,t2.qq,t2.mobile_phone,t2.email,t1.comment_number,t2.shop_introduce,t1.daijinquan,t1.fanxian,t2.weixin_erweima,t1.cat_id')->find();
         $goods['shop_introduce']=str_replace("\r", "", $goods['shop_introduce']);
         $goods['shop_introduce']=str_replace("\n", "", $goods['shop_introduce']);
+        $goods['url']['url']=urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+        $goods['url']['goods_name']=urlencode('一起网-'.$goods['cat_name'].'-'.$goods['goods_name']);
+        $goods['url']['goods_img']=urlencode('http://www.17each.com'.$goods['goods_img']);
+        $goods['url']['summary']=urlencode('一起网，您的婚庆专家。'.$goods['cat_name'].'-'.$goods['goods_name']);
         $this->assign('goods',$goods);
         $shop_introduce=strlen($goods['shop_introduce']);
         $this->assign('shop_introduce',$shop_introduce);
