4d00077b534cd3157250a308c9ba3c9548583d64
xiayuanchuan <xiayuanchuan@bravesoft.co.jp>
2016-03-01 16:09:12 +0800

    支付成功页面 同步返回      显示支付结果    同时做了更新数据工作        暂时没有到异步验证

--- Application/Home/Controller/GoodsController.class.php
+++ Application/Home/Controller/GoodsController.class.php
@@ -288,8 +288,6 @@ class GoodsController extends FontEndController {
      */
 
     public function gmcg() {
-
-//___________________________________________
         vendor('create_direct_pay_by_xia.lib.alipay_notify'); //引入第三方类库
         //计算得出通知验证结果
         $alipayNotify = new \AlipayNotify(C("ALIPAY_CONFIG"));
@@ -312,11 +310,11 @@ class GoodsController extends FontEndController {
                 $ordermodel = D('Order');
                 $order = $ordermodel->where("order_no=$out_trade_no")->find();
                 $this->assign('order', $order);
-                 $order_user_id = $order['user_id']; //登录用户无该订单权限
+                $order_user_id = $order['user_id']; //登录用户无该订单权限
                 if ($order_user_id != $_SESSION['huiyuan']['user_id']) {//登录用户无该订单权限
                     $this->error('您没有该订单权限');
                 }
-                $order_id=$order['order_id'];
+                $order_id = $order['order_id'];
                 //如果该条订单已被别人付款，提示已经被购买，返回首页
                 $goods_id = $order['goods_id'];
                 $server_day = $order['server_day'];
@@ -331,27 +329,27 @@ class GoodsController extends FontEndController {
                 $row = array(
                     'pay_status' => 1, //支付状态为支付
                     'updated' => mktime(),
-                    "pay_type"=>0,
-                    "trade_no"=>$trade_no,
-                    "pay_info"=>  serialize($_GET),
+                    "pay_type" => 0,
+                    "trade_no" => $trade_no,
+                    "pay_info" => serialize($_GET),
                 );
                 $ordermodel->where("order_id=$order_id")->save($row);
 
                 //商品表里面购买数量加1
                 $row_goods = array(
-                    'buy_number' => "buy_number" ."+ 1",
+                    'buy_number' => "buy_number" . "+ 1",
                 );
                 $goodsmodel = D('Goods');
                 $goodsmodel->where("goods_id=$goods_id")->save($row_goods);
                 $this->display('gmcg');
             } else {
-                $message= "支付遇到问题，请稍后重试！"."trade_status=" . $_GET['trade_status'];
-                $this->error($message,  U('Order/index'), 3);
+                $message = "支付遇到问题，请稍后重试！" . "trade_status=" . $_GET['trade_status'];
+                $this->error($message, U('Order/index'), 3);
             }
         } else {
             //验证失败
-            $message= "支付遇到问题，请稍后重试！";
-             $this->error($message,  U('Order/index'), 3);
+            $message = "支付遇到问题，请稍后重试！";
+            $this->error($message, U('Order/index'), 3);
         }
 
 //---------------------------------------------------------------
