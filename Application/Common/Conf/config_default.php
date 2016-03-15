d8882dcc7551fff9e92e03dcd95ae734bd5f87be
magicbirdyj <12493085@qq.com>
2016-03-15 17:13:05 +0800

    配置

--- Application/Common/Conf/config_default.php
+++ Application/Common/Conf/config_default.php
@@ -47,13 +47,16 @@ return array(
     'DB_NAME'               =>  'qdm11457371_db',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  'asd123321',          // 密码
-    'DB_PORT'               =>  '',        // 端口
+    'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'm_',    // 数据库表前缀
     'DB_PARAMS'          	=>  array(), // 数据库连接参数    
     'DB_DEBUG'  			=>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
     'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
     'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
-    
-    
+        
     'ALIPAY_CONFIG'=>$alipay_config,
+     'PAY_METHOD'=>array(
+        1=>array("name"=>"支付宝","id"=>"1","img_url"=>""),
+        2=>array("name"=>"微信支付","id"=>"2",'img_url'=>""),
+    ),
 );
