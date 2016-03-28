3967ce42ad879861bf5bfefa2b260c472455869b
magicbirdyj <12493085@qq.com>
2016-03-29 00:45:53 +0800

    会员管理

---
+++ Application/Admin/Controller/UsersmanageController.class.php
@@ -0,0 +1,23 @@
+<?php
+namespace Admin\Controller;
+use Admin\Controller;
+class UsersmanageController extends FontEndController {
+   
+    public function index(){
+        $usersmodel=D('Users');
+        $count=$usersmodel->count();
+        $page=$this->get_page($count, 10);
+        $page_foot=$page->show();//显示页脚信息
+        $list=$usersmodel->field('user_id,email,user_name,reg_time,mobile_phone,shopman_id,server_content')->select();
+        $this->assign('list',$list);
+        $this->assign('page_foot',$page_foot);
+        $this->display('index');
+    }
+    
+
+    
+    
+    
+
+
+}
